28fcb2298a1a27c999542dc0bc02b70385aeb0bf
Prince Opoku Amaning <105636490+opokuamaning@users.noreply.github.com>
2023-07-13 18:20:47 +0000

    Added the last touches

--- js/script.js
+++ js/script.js
@@ -3,20 +3,33 @@ let actualLocation = document.querySelector(".actual-location");
 let btn = document.querySelector(".btn-click");
 let celsius = document.querySelector(".wd-2");
 let cloudDetails = document.querySelector(".wd-3");
+let weatherIcon = document.querySelector(".weather-icon");
 let humidity = document.querySelector(".wd-4");
 let windSpeed = document.querySelector(".wd-5");
-const apiKey = "e484b7212dbad86fcd6fb337aa366672"
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid='+apiKey
+const apiKey = "bb19114de0d6658a7657a2811f946d50"
+
+ //`https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(input.value)}&APPID=bb19114de0d6658a7657a2811f946d50` // `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=bb19114de0d6658a7657a2811f946d50`
+
 
 btn.addEventListener("click", ()=>{
     const getLocation = input.value;
     console.log(getLocation);
     actualLocation.innerHTML = "Weather in " + getLocation;
-
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${getLocation}&appid=${apiKey}&units=metric`;
     fetch(apiURL)
     .then(response => response.json())
     .then(data => {
-        
+        let temperature = data.main.temp;
+        let cloudDetail = data.weather[0].description;
+        let icon = data.weather[0].icon;
+        let iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
+        let hum = data.main.humidity;
+        let wind = data.wind.speed;
+        celsius.innerHTML = `${temperature}\u00B0C`;
+        weatherIcon.src = iconUrl;
+        cloudDetails.innerHTML = cloudDetail
+        humidity.innerHTML = `Humidity: ${hum}`;
+        windSpeed.innerHTML = `Wind speed ${wind}km/h`
         console.log(data);
     })
     .catch(error =>{
